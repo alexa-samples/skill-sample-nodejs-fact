65173d787de28a1167f1adc85ddc407367f0f851
ygzy <ygzg@amazon.com>
2016-09-27 10:52:17 -0700

    minor change

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var languageStrings = {
             "FACTS": [
                 "A year on Mercury is just 88 days long.",
                 "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
+                "Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.",
                 "On Mars, the Sun appears about half the size as it does on Earth.",
                 "Earth is the only planet not named after a god.",
                 "Jupiter has the shortest day of all the planets.",
@@ -21,7 +21,7 @@ var languageStrings = {
                 "The Moon is moving approximately 3.8 cm away from our planet every year."
             ],
             "SKILL_NAME" : "British Space Facts",
-            "GET_FACT_MESSAGE" : "Here\'s your fact: ",
+            "GET_FACT_MESSAGE" : "Here's your fact: ",
             "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
@@ -45,7 +45,7 @@ var languageStrings = {
                 "The Moon is moving approximately 3.8 cm away from our planet every year."
             ],
             "SKILL_NAME" : "American Space Facts",
-            "GET_FACT_MESSAGE" : "Here\'s your fact: ",
+            "GET_FACT_MESSAGE" : "Here's your fact: ",
             "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
@@ -95,7 +95,7 @@ var handlers = {
     },
     'GetFact': function () {
         // Get a random space fact from the space facts list
-        // Use this.localize()/this.i18n.t() to get corresponding language data
+        // Use this.t() to get corresponding language data
         var factArr = this.t('FACTS');
         var factIndex = Math.floor(Math.random() * factArr.length);
         var randomFact = factArr[factIndex];
