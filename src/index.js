813ec3f64eba8cfc6eb235798aeed3e4edd4c963
ygzy <ygzg@amazon.com>
2016-09-27 10:47:20 -0700

    [FactSkillSample] Add German String

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var languageStrings = {
             "FACTS": [
                 "A year on Mercury is just 88 days long.",
                 "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.",
+                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
                 "On Mars, the Sun appears about half the size as it does on Earth.",
                 "Earth is the only planet not named after a god.",
                 "Jupiter has the shortest day of all the planets.",
@@ -21,7 +21,7 @@ var languageStrings = {
                 "The Moon is moving approximately 3.8 cm away from our planet every year."
             ],
             "SKILL_NAME" : "British Space Facts",
-            "GET_FACT_MESSAGE" : "Here's your fact: ",
+            "GET_FACT_MESSAGE" : "Here\'s your fact: ",
             "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
@@ -45,10 +45,34 @@ var languageStrings = {
                 "The Moon is moving approximately 3.8 cm away from our planet every year."
             ],
             "SKILL_NAME" : "American Space Facts",
-            "GET_FACT_MESSAGE" : "Here's your fact: ",
+            "GET_FACT_MESSAGE" : "Here\'s your fact: ",
             "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
+        }
+    },
+    "de-DE": {
+        "translation": {
+            "FACTS": [
+                "Ein Jahr dauert auf dem Merkur nur 88 Tage.",
+                "Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.",
+                "Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.",
+                "Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.",
+                "Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.",
+                "Jupiter hat den kürzesten Tag aller Planeten.",
+                "Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.",
+                "Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.",
+                "Die Sonne ist eine fast perfekte Kugel.",
+                "Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.",
+                "Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.",
+                "Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.",
+                "Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr."
+            ],
+            "SKILL_NAME" : "Weltraumwissen auf Deutsch",
+            "GET_FACT_MESSAGE" : "Hier sind deine Fakten: ",
+            "HELP_MESSAGE" : "Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?",
+            "HELP_REPROMPT" : "Wie kann ich dir helfen?",
+            "STOP_MESSAGE" : "Auf Wiedersehen!"
         }
     }
 };
@@ -71,7 +95,7 @@ var handlers = {
     },
     'GetFact': function () {
         // Get a random space fact from the space facts list
-        // Use this.t() to get corresponding language data
+        // Use this.localize()/this.i18n.t() to get corresponding language data
         var factArr = this.t('FACTS');
         var factIndex = Math.floor(Math.random() * factArr.length);
         var randomFact = factArr[factIndex];
