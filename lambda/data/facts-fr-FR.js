aaaa417ee70d2ea62918af73e0a9f803bf3e8145
Paul-Louis NECH <1821404+PLNech@users.noreply.github.com>
2018-06-15 08:17:03 +0000

    Create facts-fr-FR.js
    French translation.

---
+++ lambda/data/facts-fr-FR.js
@@ -0,0 +1,21 @@
+var data = [
+ "Une année sur Mercure ne dure que 88 jours.",
+     "Vénus est plus loin du Soleil que Mercure, mais elle est bien plus chaude.",
+     "Vénus tourne dans un sens anti-horaire, peut-être à cause d'une collision passée avec un astéroïde.",
+     "Sur Mars, le Soleil a l'air environ deux fois moins grand que sur Terre.",
+     "La Terre est la seule planète qui ne porte pas le nom d'un dieu.",
+     "Jupiter a le jour le plus court de toutes les planètes.",
+     "La Voie Lactée va entrer en collision avec la galaxie d'Andromède dans environ 5 milliards d'années.",
+     "Le soleil contient 99,86% de la masse dans le système solaire.",
+     "Le Soleil est une sphère presque parfaite.",
+     "Une éclipse solaire totale peut se produire une fois tous les 1 à 2 ans, ce qui en fait un événement rare!",
+     "Saturne renvoie dans l'espace deux à trois fois plus d'énergie qu'elle n'en reçoit du soleil.",
+     "La température du Soleil peut atteindre 15 millions de degrés Celsius.",
+     "La Lune s'éloigne de 3,8 cm de notre planète chaque année."
+]
+
+var SKILL_NAME = "Faits sur l'Espace";
+var GET_FACT_MESSAGE = "Voilà un fait: ";
+var HELP_MESSAGE = "Tu peux demander un fait sur l'espace, ou dire au revoir... Est-ce que je peux t'aider ?";
+var HELP_REPROMPT = "Est-ce que je peux t'aider ?";
+var STOP_MESSAGE = "Au revoir!";
