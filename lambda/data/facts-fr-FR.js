d6c281fd13cc9f5bbbf0bd2fd741b027e760be16
Adrien Clerbois <adrien@aclerbois.be>
2018-06-19 20:28:22 +0200

    Add french facts and models

---
+++ lambda/data/facts-fr-FR.js
@@ -0,0 +1,21 @@
+var data = [
+    "Une année sur Mercure ne dure que 88 jours.",
+    "Bien qu'elle soit plus éloignée du Soleil, Vénus connaît des températures plus élevées que Mercure.",
+    "Vénus tourne dans le sens inverse des aiguilles d'une montre, peut-être à cause d'une collision passée avec un astéroïde.",
+    "Sur Mars, le Soleil apparaît deux fois moins grand que sur Terre.",
+    "La Terre est la seule planète qui ne porte pas le nom d'un dieu.",
+    "Jupiter a le jour le plus court de toutes les planètes.",
+    "La Voie lactée entrera en collision avec la galaxie d'Andromède dans environ 5 milliards d'années.",
+    "Le Soleil contient 99,86 % de la masse du système solaire.",
+    "Le Soleil est une sphère presque parfaite.",
+    "Une éclipse solaire totale peut se produire une fois tous les 1 à 2 ans. Cela en fait un événement rare.",
+    "Saturne rayonne deux fois et demie plus d'énergie dans l'espace qu'elle n'en reçoit du soleil.",
+    "La température à l'intérieur du Soleil peut atteindre 15 millions de degrés Celsius.",
+    "La Lune s'éloigne d'environ 3,8 cm de notre planète chaque année."
+];
+
+var SKILL_NAME = "Faits sur l'espace européen";
+var GET_FACT_MESSAGE = "Voici votre fait : ";
+var HELP_MESSAGE = "Vous pouvez me dire un fait spatial, ou, vous pouvez dire sortir.... En quoi puis-je vous aider ?";
+var HELP_REPROMPT = "En quoi puis-je vous aider ?";
+var STOP_MESSAGE = "Au revoir!";
