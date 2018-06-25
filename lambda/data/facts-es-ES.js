bb0751cac8d79ddda07e9aec681e1230b01b41ec
maneta <dcesario@redhat.com>
2018-06-25 23:55:48 +0200

    Spanish translations of the spacial facts

---
+++ lambda/data/facts-es-ES.js
@@ -0,0 +1,22 @@
+var data = [
+    "Un año en Mercurio tiene sólo 88 días de duración.",
+    "A pesar de estar más alejado del Sol, Venus experimenta temperaturas más altas que Mercurio.",
+    "Venus gira en sentido antihorario, posiblemente debido a una colisión en el pasado con un asteroide.",
+    "En Marte, el Sol aparece aproximadamente la mitad del tamaño que en la Tierra.",
+    "La Tierra es el único planeta que no tiene el nombre de un dios.",
+    "Júpiter tiene el día más corto de todos los planetas.",
+    "La galaxia de la Vía Láctea colisionará con la Galaxia de Andrómeda en unos 5 mil millones de años.",
+    "El Sol contiene 99.86% de la masa en el Sistema Solar.",
+    "El Sol es una esfera casi perfecta.",
+    "Un eclipse solar total puede ocurrir una vez cada 1 o 2 años. Esto los convierte en un evento raro.",
+    "Saturno irradia dos veces y media más energía en el espacio de lo que recibe del sol.",
+    "La temperatura dentro del Sol puede alcanzar los 15 millones de grados Celsius.",
+    "La Luna se mueve aproximadamente a 3.8 cm de nuestro planeta cada año."
+];
+
+var SKILL_NAME = "Datos Espaciales Español";
+var GET_FACT_MESSAGE = "Aquí está su hecho: ";
+var HELP_MESSAGE = "Puedes decirme un hecho espacial, o puedes decir salir ... ¿En qué puedo ayudarte?";
+var HELP_REPROMPT = "¿En qué puedo ayudarte?";
+var STOP_MESSAGE = "¡Adiós!";
+
