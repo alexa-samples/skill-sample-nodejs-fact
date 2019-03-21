2bc49d58f5d8f8b3574f82dadea26ada7d5e4ae7
Rafael Miranda <rafmir@amazon.de>
2019-03-21 15:12:21 +0100

    Adding pt_BR support 🎉

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -344,6 +344,27 @@ const jpjpData = {
   },
 };
 
+const ptData = {
+  translation: {
+    SKILL_NAME: 'Fatos Espaciais',
+    GET_FACT_MESSAGE: 'Aqui vai: ',
+    HELP_MESSAGE: 'Você pode me perguntar por um fato interessante sobre o espaço, ou, fexar a skill. Como posso ajudar?',
+    HELP_REPROMPT: 'O que vai ser?',
+    FALLBACK_MESSAGE: 'A skill fatos espaciais não tem uma resposta para isso. Ela pode contar informações interessantes sobre o espaço, é só perguntar. Como posso ajudar?',
+    FALLBACK_REPROMPT: 'What can I help you with?',
+    ERROR_MESSAGE: 'Desculpa, algo deu errado.',
+    STOP_MESSAGE: 'Tchau!',
+    FACTS:
+      [
+        'Um ano em Mercúrio só dura 88 dias.',
+        'Apesar de ser mais distante do sol, Venus é mais quente que Mercúrio.',
+        'Visto de marte, o sol parece ser metade to tamanho que nós vemos da terra.',
+        'Júpiter tem os dias mais curtos entre os planetas no nosso sistema solar.',
+        'O sol é quase uma esfera perfeita.',
+      ],
+  },
+};
+
 // constructs i18n and l10n data structure
 // translations for this sample can be found at the end of this file
 const languageStrings = {
@@ -364,4 +385,6 @@ const languageStrings = {
   'it-IT': ititData,
   'ja': jpData,
   'ja-JP': jpjpData,
+  'pt': ptData,
+  'pt-BR': ptData
 };
