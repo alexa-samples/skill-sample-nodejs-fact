88e8001052a864075f334f41ee2942c1b4248bc8
Gabriel Castello <34844519+GabrielCastello@users.noreply.github.com>
2020-03-04 21:29:56 -0300

    corrected grammar error at ptData
    Wrong: fexa
    Correct: fechar

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -407,7 +407,7 @@ const ptData = {
   translation: {
     SKILL_NAME: 'Fatos Espaciais',
     GET_FACT_MESSAGE: 'Aqui vai: ',
-    HELP_MESSAGE: 'Você pode me perguntar por um fato interessante sobre o espaço, ou, fexar a skill. Como posso ajudar?',
+    HELP_MESSAGE: 'Você pode me perguntar por um fato interessante sobre o espaço, ou, fechar a skill. Como posso ajudar?',
     HELP_REPROMPT: 'O que vai ser?',
     FALLBACK_MESSAGE: 'A skill fatos espaciais não tem uma resposta para isso. Ela pode contar informações interessantes sobre o espaço, é só perguntar. Como posso ajudar?',
     FALLBACK_REPROMPT: 'Eu posso contar fatos sobre o espaço. Como posso ajudar?',
